fc5603158f0199da44f363ffbba5448b2ad757ea
shigimcp <shigimcp@hotmail.com>
2020-10-10 15:14:11 -0400

    FINALLY got something to work!

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -29,7 +29,8 @@ const urls = ['night', 'city', 'morning', 'tubes', 'woods', 'beach'].map(
 // const logoUrl = './images/shigeru_logo_test.svg';
 
 // const logoUrls = ['shigeru_logo_test', 'shigeru_logo_transparent'].map(
-const logoUrls = ['arrow_lf', 'arrow_rt', 'badge_flash', 'badge_html5', 'badge_mobile', 'badge_video', 'curl_k', 'curl', 'grid', 'shigeru_logo_ko_rev', 'shigeru_logo_ko', 'shigeru_logo_rev', 'shigeru_logo', 'thoughtBubble'].map(
+// const logoUrls = ['arrow_lf', 'arrow_rt', 'badge_flash', 'badge_html5', 'badge_mobile', 'badge_video', 'curl_k', 'curl', 'grid', 'shigeru_logo_ko_rev', 'shigeru_logo_ko', 'shigeru_logo_rev', 'shigeru_logo', 'thoughtBubble'].map(
+const logoUrls = ['shigeru_logo_ko_rev', 'shigeru_logo_ko', 'shigeru_logo_rev', 'shigeru_logo'].map(
     // (name) => `https://www.shigimcp.com/Xstage/shigimcp_2020_react/img/0elements/${name}.svg`
     // (name) => `http://www.shigimcp.com/Xstage/shigimcp_2020_react/img/0elements/${name}.svg`
 
@@ -38,10 +39,14 @@ const logoUrls = ['arrow_lf', 'arrow_rt', 'badge_flash', 'badge_html5', 'badge_m
     // (name) => `https://github.com/shigimcp/threejs-experiment-02/tree/main/src/images/${name}.svg`
     // (name) => `https://shigimcp.github.io/threejs-experiment-02/images/${name}.svg`
     // (name) => `https://github.com/shigimcp/threejs-experiment-02/blob/master/${name}.svg?raw=true`
-    (name) => `https://raw.githubusercontent.com/shigimcp/threejs-experiment-02/images/${name}.svg`
+    // (name) => `https://raw.githubusercontent.com/shigimcp/threejs-experiment-02/images/${name}.svg`
+    (name) => `https://raw.githubusercontent.com/shigimcp/threejs-experiment-02/main/src/.github/images/${name}.svg`
+    // (name) => `https://raw.githubusercontent.com/shigimcp/threejs-experiment-02/gh-pages/.github/images/kawaii02_dirty_anim03_1920x1080_16bit.gif`
 );
 
-
+// REF: https://stackoverflow.com/questions/61537403/how-to-host-image-at-https-user-images-githubusercontent-com-path-filename
+// https://raw.githubusercontent.com/{github_user_name}/{repo_name}/{branch}/.github/images/{aset_name}.{asset_extension}
+// https://raw.githubusercontent.com/shigimcp/threejs-experiment-02/main/.github/images/{aset_name}.{asset_extension}
 
 
 function Shape({ shape, rotation, position, color, opacity, index }) {
